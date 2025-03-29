21cb4321f7da5d1eb6d085f52cb4a296ace8ed35
Badalkapoor-arc <badalkapoorBK1198@gmail.com>
2025-03-29 21:08:39 +0530

    password authentication

--- backend.js
+++ backend.js
@@ -1,8 +1,11 @@
 let menuBTN = document.querySelector('.menu');
 let menuBar = document.querySelector('#menuBar');
-let submitBTN = document.querySelector('#btn');
 let closeBTN = document.querySelector('.closeMenu');
 let menuBar1 = document.querySelector('#menuBar1');
+let password = document.querySelector(".password");
+let submitBTN = document.querySelector('#btn');
+let form = document.querySelector("form");
+let key = "T20P163";
 menuBTN.addEventListener('click',()=>{
     menuBar.setAttribute("id","menuBar1");
 });
@@ -26,6 +29,13 @@ document.querySelector('.student').addEventListener('click',()=>{
 document.querySelector('.about').addEventListener('click',()=>{
     window.location.href = "about.html";
 });
-submitBTN.addEventListener("click",()=>{
-    window.location.href = "infoCheck.html";
+submitBTN.addEventListener("click", () => {
+    if (password.value.toString() === key.toString()) {
+        console.log(password.value.toString());
+        form.method="form";
+        form.action="infoCheck.html";
+    } else {
+        alert("Incorrect password or password field is empty");
+    }
 });
+console.log(key.toString());
